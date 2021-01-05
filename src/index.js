33076a9e977aebe4a24c6538212013ebdbd93b77
ka52k <76809740+ka52k@users.noreply.github.com>
2021-01-05 17:48:27 +0900

    20210105－3変更

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 import React from "react";
 import ReactDom from "react-dom";
+import App from "./App";
 
-const App = () => {
-  return null;
-};
+// const App = () => {
+//   return (
+//     // <div>
+//     //   <h1>こんにちは！</h1>
+//     //   <p>お元気ですか</p>
+//     // </div>
+
+//     // <React.Fragment>    
+//     //   <h1>こんにちは！</h1>
+//     //   <p>お元気ですか</p>
+//     // </React.Fragment>
+    
+//     <>
+//       <h1>こんにちは！</h1>
+//       <p>お元気ですか</p>
+//     </>
+//   );
+// };
 
 ReactDom.render(<App />, document.getElementById("root"));
